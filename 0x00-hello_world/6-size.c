9a009f2ec6720a2583f6631931f06fc7944c6674
sayedhany <sayedh@mail.ru>
2023-02-16 11:52:38 -0800

    add size file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,7 @@
 
 /**
   * main - print size of data types
-  * return - return zero
+  * Return: return zero
   */
 int main(void)
 {
@@ -11,11 +11,11 @@ int main(void)
 	long int n3;
 	long long int n4;
 	float n5;
+
 	printf("Size of a char: %lu byte(s)\n", sizeof(n1));
 	printf("Size of an int: %lu byte(s)\n", sizeof(n2));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(n3));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(n4));
-	printf("Size of a float: %lu byte(s)\n", sizeof(n5));
-	
-	return 0;
+	printf("Size of a float: %lu byte(s)\n", sizeof(n5));	
+	return (0);
 }
