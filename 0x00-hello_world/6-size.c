a154189770d8c4652cdf238d5a3664f1e86f53a2
sayedhany <sayedh@mail.ru>
2023-02-16 11:53:38 -0800

    solved

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,7 +11,6 @@ int main(void)
 	long int n3;
 	long long int n4;
 	float n5;
-
 	printf("Size of a char: %lu byte(s)\n", sizeof(n1));
 	printf("Size of an int: %lu byte(s)\n", sizeof(n2));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(n3));
