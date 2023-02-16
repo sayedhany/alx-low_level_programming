25f1d96409880266dc45b12b1a450884f2d1645a
sayedhany <sayedh@mail.ru>
2023-02-16 11:23:07 -0800

    solved

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
-  *main - prints the statement "Programming is like building a multilingual puzzle
+  *main - prints "Programming is like building a multilingual puzzle
   * Return: 0 if exited properly, non zero otherwise
   */
 int main(void)
