b4dab7a29e3d6ece7dc1f8c922eb39edbdc8f96b
sayedhany <sayedh@mail.ru>
2023-02-17 11:24:33 -0800

    solved

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+  *main - this main func print alphapts
+  *Return: return 0 if success
+  */
+int main(void)
+{
+	char ch;
+
+	ch = 'a';
+	while (ch <= 'z')
+	{
+		printf("%c",ch);
+		ch++;
+	}
+	printf("\n");
+	return (0);
+}
