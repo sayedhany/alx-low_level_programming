293bf0e73b04f437be3ad861227e0ee04855f8e5
sayedhany <sayedh@mail.ru>
2023-02-21 12:41:39 -0800

    solved

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,9 +10,9 @@ void times_table(void)
 	{
 		for (j = 0; j < 10; j++)
 		{
-
-			/*_putchar(((i * j) / 10) + '0');*/
-			if (i <= 9 || j <= 9)
+			
+			_putchar(((i * j) / 10) + '0');
+			if (i > 9 || j > 9)
 			{
 				_putchar(((i * j) % 10) + '0');
 			}
