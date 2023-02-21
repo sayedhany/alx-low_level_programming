8528dbb1ee93c7f5df60181f3902b372ee36244d
sayedhany <sayedh@mail.ru>
2023-02-21 07:12:50 -0800

    add alphabet file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+  *main - print alphapet lowercase
+  *Return: return 0 if success
+  */
+
+int main(void)
+{
+	char ch = 'a';
+	
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+	return (0);
+}
