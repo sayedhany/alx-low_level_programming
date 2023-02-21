2ee6f1146396b25ecd39fd4570e1ea5de0d08caf
sayedhany <sayedh@mail.ru>
2023-02-21 06:04:38 -0800

    added

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,10 +10,10 @@ int main(void)
 	char ch[] = "_putchar";
 	int num;
 
-	for(num = 0; num < 8; num++)
+	for (num = 0; num < 8; num++)
 	{
 		_putchar(ch[num]);
 	}
 	_putchar('\n');
-	return (0);		
+	return (0);
 }
