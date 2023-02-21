716f89646119f5cb9c4ff1c1aa97dc09015cfa1a
sayedhany <sayedh@mail.ru>
2023-02-21 10:55:20 -0800

    solved

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,13 +7,13 @@
   */
 int print_last_digit(int num)
 {
-	int last_digit;
+	char last_digit;
 
 	last_digit = num % 10;
 	if (last_digit < 0)
 	{
 		last_digit = -1 * last_digit;
 	}
-	_putchar('last_digit');
-	return ('last_digit');
+	_putchar(last_digit);
+	return (last_digit);
 }
