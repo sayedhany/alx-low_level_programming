f074ce7a2e24a9bb434ec635eec117ddd929539c
sayedhany <sayedh@mail.ru>
2023-02-21 12:26:28 -0800

    solved

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,8 +16,11 @@ void times_table(void)
 			{
 				_putchar(((i * j) % 10) + '0');
 			}
-			_putchar(',');
-			_putchar(' ');
+			if (j < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
 		_putchar('\n');
 	}
