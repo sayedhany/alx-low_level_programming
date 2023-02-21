dc09ccdf5bbd0510db7540bb4075057401d210ff
sayedhany <sayedh@mail.ru>
2023-02-21 10:45:52 -0800

    solved

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+  *print_last_digit - print last digit
+  *@num: number to checked
+  *
+  *Return: return last number
+  */
+int print_last_digit(int num)
+{
+	int last_digit;
+
+	last_digit = num / 10;
+	if (last_digit < 0)
+	{
+		last_digit = -1 * last_digit;
+	}
+	_putchar(last_digit);
+	return (last_digit);
+}
