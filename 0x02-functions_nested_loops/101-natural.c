d93f32db511b0f3b6b96401656417526d0f0eac6
sayedhany <sayedh@mail.ru>
2023-02-21 13:41:42 -0800

    solved

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void main(void)
+{
+	int sum, i;
+
+	sum = 0;
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum = sum + i;
+		}
+	}
+	printf("%d\n", sum);
+}
