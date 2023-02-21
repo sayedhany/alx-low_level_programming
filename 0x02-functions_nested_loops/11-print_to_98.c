16a416bebe18038e220e783dcf312e2a39515934
sayedhany <sayedh@mail.ru>
2023-02-21 13:23:58 -0800

    solved

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,17 @@
+#include <main.h>
+/**
+  *print_to_98 - print numbers to 98
+  *
+  */
+void print_to_98(int n)
+{
+	int num;
+	for (num = n; num <= 98; num++)
+	{
+		putchar((num / 10) + '0');
+		putchar((num % 10) + '0');
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+}
