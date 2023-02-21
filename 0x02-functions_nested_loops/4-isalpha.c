41f2e2ff966526da96fc0d0a8f967b5ffd01adf3
sayedhany <sayedh@mail.ru>
2023-02-21 10:04:53 -0800

    solved

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * _isaplpha - check if the char is alphabet or not
+  * @c: char which will be checked
+  *
+  * Return: i if alph 0 or not
+  */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	return (0);
+}
