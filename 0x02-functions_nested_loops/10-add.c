c8fa9ebe66859784d0e1b53e1e4ef6c4ca96bd53
sayedhany <sayedh@mail.ru>
2023-02-21 13:15:04 -0800

    solved

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include <main.h>
+/** 
+  *add - print sum of two numbers
+  *@x - number one
+  *@y - number two
+  *
+  *Return: the sum of two number
+  */
+int add(int x, int y)
+{
+	return (x + y);
+}
