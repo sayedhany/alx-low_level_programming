695c19132b7778d1744100bf03b41c6193b5eace
sayedhany <sayedh@mail.ru>
2023-02-21 10:24:44 -0800

    solved

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+  * print_sign - print + if number is beager then 0 - if less then 0 0 if 0
+  *@n: number which we check
+  *
+  *Return: 1 if more then 1
+  */
+int print_sign(int n)
+{
+	if (n >= 1)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if ( n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
