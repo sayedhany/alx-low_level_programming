236fcec09a4525018e7d8128ac5e4015a370cafc
sayedhany <sayedh@mail.ru>
2023-02-21 10:34:38 -0800

    solved

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+  *_abs - abs the value
+  *@num: number to checked 
+  *
+  *Return absolute numnber
+  */
+int _abs(int num)
+{
+	if (num < 0)
+	{
+		return (-1 * num);
+	}
+	return num;
+}
