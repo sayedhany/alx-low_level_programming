402eef0a205a36361acbd63c20e552e2088233d5
sayedhany <sayedh@mail.ru>
2023-02-23 10:11:34 -0800

    solved

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+  * main - print fizz if 3 buzz if 5 fizzbuzz if both
+  *
+  * Return: 0 if success
+  */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 100; i++)
+	{
+		if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (i % 5 == 0 && i % 3 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	_putchar('\n');
+	return (0);
+}
