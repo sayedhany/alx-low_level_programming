949a633189dfb4d96ae6884dbbc2b7b1ab87c385
sayedhany <sayedh@mail.ru>
2023-02-27 10:48:03 -0800

    add file

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+  * rest_to_98 - get address of variable and change its value
+  * @n: address of variable which we will change it
+  */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+/*
+    * File: main.h
+     * Auth: Sayed Hany
+      * Desc: Header file containing prototypes for all functions
+       *       used in the 0x03-more_functions_nested_loops directory.
+*/
+int _putchar(char c);
+void reset_to_98(int *n);
+
+#endif
