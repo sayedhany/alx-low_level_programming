79f16b54ceb0ac40b517f3967beb0a5ae4e38e07
sayedhany <sayedh@mail.ru>
2023-02-27 11:06:36 -0800

    add file

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+  * _strlin - give us the number of string
+  * s: input
+  * Return: the length of s
+  */
+
+int _strlin(char *s)
+{
+	int i;
+	int count;
+
+	count = 0;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		count++;
+	}
+	return count
+}
