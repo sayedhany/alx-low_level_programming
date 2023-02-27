f4be1b66c77d1bc3655ea8fa71170dee1722af13
sayedhany <sayedh@mail.ru>
2023-02-27 15:47:00 -0800

    solved

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,12 @@
+#include <main.h>
+/**
+  * _strbpy - concat two string
+  * @dest: first string
+  * @src: second string
+  *
+  *Return: the pointer to dest
+  */
+char *_strcpy(char *dest, char *src)
+{
+	return *(strcpy(dest, src));
+}
