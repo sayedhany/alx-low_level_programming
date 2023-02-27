f051d9b40136cf225a08a8cd6c9dae695ec3250e
sayedhany <sayedh@mail.ru>
2023-02-27 10:51:10 -0800

    add file

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-  * rest_to_98 - get address of variable and change its value
+  * reset_to_98 - get address of variable and change its value
   * @n: address of variable which we will change it
   */
 void reset_to_98(int *n)
