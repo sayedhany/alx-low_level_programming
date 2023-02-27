5c24a2e398583208f8be0d9dff5726c84b400f36
sayedhany <sayedh@mail.ru>
2023-02-27 11:28:33 -0800

    add file

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,8 +7,15 @@
 void print_rev(char *s)
 {
 	int i;
+	int j;
+	int count;
 
-	for (i = strlen(s); i >= 0; i--)
+	for (j = 0; s[j] != '\0'; j++)
+	{
+		count++
+	}
+
+	for (i = count; i >= 0; i--)
 	{
 		_putchar(s[i]);
 	}
