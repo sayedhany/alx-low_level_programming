e9d5248e0fb25208d385e4c1ba6803632294fd8b
sayedhany <sayedh@mail.ru>
2023-03-01 03:33:02 -0800

    sloved

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strct - append string src to dest string
+ * @dest: string
+ * @src: string
+ *  Return: ponting to resulting string
+ */
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
