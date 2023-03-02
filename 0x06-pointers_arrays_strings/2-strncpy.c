1cc191e3fd5d8316176d73e068c568433be3bc09
sayedhany <sayedh@mail.ru>
2023-03-02 04:57:10 -0800

    solved

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *_strncpy - copy number of strings from src to dest
+ * @dest: string
+ * @src: string
+ * @n: number of strings
+ * Return: address to copy string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int *result;
+	int i;
+
+	i = 0;
+	result = dest;
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (result);
+}
