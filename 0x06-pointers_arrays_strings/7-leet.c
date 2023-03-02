161c5a900c7dd5b3177a100e07f4bec75e838079
sayedhany <sayedh@mail.ru>
2023-03-02 13:43:55 -0800

    solved

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,15 +7,11 @@
 char *leet(char *s)
 {
 	int i, j;
-	char *result;
-	char *leetspeak[];
-	char small[];
-	char capital[];
+	char *result = s;
+	char *leetspeak[] = {"4", "3", "0", "7", "1"};
+	char small[] = {'a', 'e', 'o', 't', 'l'};
+	char capital[] = {'A', 'E', 'O', 'T', 'L'};
 
-	leetspeak[] = {"4", "3", "0", "7", "1"};
-	result = s;
-	small[] = {'a', 'e', 'o', 't', 'l'};
-	capital[] = {'A', 'E', 'O', 'T', 'L'};
 	for (i = 0; i < 5; i++)
 	{
 		for (j = 0; s[j]; j++)
