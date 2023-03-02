92a1ed6759fa407889dfb46fcdf64b107079327c
sayedhany <sayedh@mail.ru>
2023-03-02 09:09:48 -0800

    add file

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,11 +7,17 @@
 char *cap_string(char *s)
 {
 	int i;
+	int cap;
 
+	i = 0;
+	cap = 1;
 	while (s[i] != '\0')
 	{
-		if (s[i] == 0)
-			s[i + 1] -= 32;
+		if (cap && (s[i] >= 'a' && s[i] <= 'z'))
+		{
+			s[i] -= ('a' - 'A');
+		}
+		cap = (s[i] == ' ' || s[i] == '\n' || s[i] == '\t' || s[i] == '.');
 		i++;
 	}
 	return (s);
