92d2c26a2e0443b9e62ac47d58f94ecc6bd9bcbf
sayedhany <sayedh@mail.ru>
2023-03-06 15:42:10 -0800

    solve

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,17 +9,20 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	unsigned int i, j, k;
-
-	for (i = 0; haystack[i] != '\0'; i++)
+	while (*haystack != '\0')
 	{
-		for (j = i, k = 0; needle[k] != '\0'; j++, k++)
+		char *duplicate = haystack;
+
+		while (*haystack == *needle && *haystack != '\0' && *needle != '\0')
 		{
-			if (haystack[j] != needle[k] || haystack[j] == '\0')
-				break;
+			needle++;
+			haystack++;
 		}
-		if (needle[k] == '\0')
-			return (haystack + i);
+		if (!*needle != '\0')
+		{
+			return (duplicate);
+		}
+		haystack++;
 	}
-	return (NULL);
+	return (0);
 }
