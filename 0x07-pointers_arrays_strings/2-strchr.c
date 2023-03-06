2632bff74d46f28b8ea36d9d3bb6f5beac357a08
sayedhany <sayedh@mail.ru>
2023-03-06 13:48:35 -0800

    solve

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strchr -check character
+ * @s: string of characters
+ * @c: char
+ * Return: string
+ */
+char *_strchr(char *s, char c)
+{
+	unsigned int i;
+
+	for (i = 0; s[i] != '\0' && s[i] == c; i++)
+	{
+		return (&s[i]);
+	}
+}
