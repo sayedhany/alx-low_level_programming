2dca1bd6aae1d23b2cc7d4f6026cb01ecfd2263a
sayedhany <sayedh@mail.ru>
2023-03-06 13:54:17 -0800

    solve

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,9 +10,13 @@ char *_strchr(char *s, char c)
 {
 	unsigned int i;
 
-	for (i = 0; s[i] != '\0' && s[i] == c; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		return (s[i]);
+		if (s[i] == c)
+		{
+			s++;
+			return (s); 
+		}
 	}
 	return (NULL);
 }
