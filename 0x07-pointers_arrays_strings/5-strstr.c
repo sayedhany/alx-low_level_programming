c3981513c5dfae54e708fb0003ad51c373b5780f
sayedhany <sayedh@mail.ru>
2023-03-06 15:27:22 -0800

    solve

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strstr - string
+ * @haystack: string
+ * @needle: string
+ * Return: string
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int i, j;
+
+	for (i = 0; needle[i] != '\0'; i++)
+	{
+		for (j = 0; haystack[j] != '\0'; j++)
+		{
+			if (haystack[j] == needle[i])
+			{
+				return (&haystack[i]);
+			}
+		}
+	}
+	return (0);
+}
