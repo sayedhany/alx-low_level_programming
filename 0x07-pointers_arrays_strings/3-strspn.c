c393f6cee5ec2035711e115f2b903db7cbfae27e
sayedhany <sayedh@mail.ru>
2023-03-06 14:42:56 -0800

    solve

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -13,7 +13,7 @@ unsigned int _strspn(char *s, char *accept)
 	count = 0;
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; accept[i] != '\0'; j++)
+		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
 			{
