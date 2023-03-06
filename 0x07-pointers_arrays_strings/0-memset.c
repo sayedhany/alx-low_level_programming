40b608bdb69d987edbb3af66af140e9958b5911a
sayedhany <sayedh@mail.ru>
2023-03-06 12:04:48 -0800

    solved

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memset - fill s with n number of b
+ * @s: address of s
+ * @b: char
+ * @n: number of things
+ * Return: s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b
+	}
+
+	return (s);
+}
