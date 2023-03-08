cb35db60ff552e8fb32ee1b4ce057088dabb30bd
sayedhany <sayedh@mail.ru>
2023-03-08 12:40:36 -0800

    solved

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,13 +4,12 @@
  * _puts_recursion - print string
  * @s: string
  */
-void _puts_string(char *s)
+void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s != '\0')
 	{
-		_putchar('\0');
-		return;
+		_putchar(*s);
+		_puts_recursion(s++);
 	}
-	_putchar(*s);
-	_puts_string(s++);
+	_putchar('\0');
 }
