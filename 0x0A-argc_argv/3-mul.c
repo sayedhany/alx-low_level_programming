4cdbbbd20d7ac581e13b230433c7662ba3bb7740
sayedhany <sayedh@mail.ru>
2023-03-10 12:13:09 -0800

    solved

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print multiplication of two numbers
+ * @argc: number of arguments
+ * @argv: arguments of vector
+ * Return: 0 if success
+ */
+int main(int argc, char *argv[])
+{
+	int i, res = 1;
+
+	if (argc == 1)
+		printf("Error\n");
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			res *= atoi(argv[i]);	
+		}
+		printf("%d\n", res);
+	}
+
+	return (0);
+}
