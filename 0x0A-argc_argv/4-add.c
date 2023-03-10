49f54a820e848e08f2eb316a99fa7c46597701dc
sayedhany <sayedh@mail.ru>
2023-03-10 12:45:16 -0800

    solved

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,21 +10,23 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, sum = 0;
+	int i, j, sum = 0;
 
-	if (argc == 1)
+	if (argc <= 1)
 		printf("0\n");
 	else
 	{
 		for (i = 1; i < argc; i++)
 		{
-			if (isdigit(argv[i]))
-				sum += atoi(argv[i]);
-			else
+			for (j = 0; argv[i][j]; j++)
 			{
-				printf("Error\n");
-				return (1);
+				if (argv[i][j] < '0' || argv[i][j] > '9')
+				{
+					printf("Error\n");
+					return (1);
+				}
 			}
+			sum += atoi(argv[i]);
 		}
 		printf("%d\n", sum);
 	}
