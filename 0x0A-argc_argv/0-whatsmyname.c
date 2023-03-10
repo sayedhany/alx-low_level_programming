9b61feeb387309ba6f5d9d6d6c0983db56ba2817
sayedhany <sayedh@mail.ru>
2023-03-10 11:27:48 -0800

    solved

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print the name of program
+ * @argc: number of program arguments
+ * @argv: pointers of string
+ * Return: 0 if seccess
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", *argv);
+}
