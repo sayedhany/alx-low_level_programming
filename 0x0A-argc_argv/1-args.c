c119a21e273d14204b750f25597eaf1aa657e9dd
sayedhany <sayedh@mail.ru>
2023-03-10 11:59:36 -0800

    solved

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,7 +7,7 @@
  * @argv: argument of vector
  * Return: 0 if success
  */
-int main(int __attribute__((unused)) argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
 	return (0);
