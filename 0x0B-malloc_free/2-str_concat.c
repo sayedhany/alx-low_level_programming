8159b8d63130b96aed8fe2ce3a3d2c66822b8327
sayedhany <sayedh@mail.ru>
2023-03-13 11:34:08 -0700

    solved

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,6 +16,8 @@ char *str_concat(char *s1, char *s2)
 	len1 = strlen(s1);
 	len2 = strlen(s2);
 	s = malloc(((len1 + len2) * sizeof(char)) + 1);
+	if (s = NULL)
+		return (0);
 	for (i = 0; i < len1; i++)
 		s[i] = s1[i];
 	for (j = 0; j < len2; j++)
