b0a46a47d676e68eb2b2574f3c4660b8115abbf7
sayedhany <sayedh@mail.ru>
2023-03-13 11:37:52 -0700

    solved

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,8 +13,21 @@ char *str_concat(char *s1, char *s2)
 	int len1, len2, i, j;
 	char *s;
 
-	len1 = strlen(s1);
-	len2 = strlen(s2);
+	if (s1 == NULL)
+	{
+		len2 = strlen(s2);
+		len1 = 0;
+	}
+	else if (s2 == NULL)
+	{
+		len1 = strlen(s1);
+		len2 = 0;
+	}
+	else
+	{
+		len1 = strlen(s1);
+		len2 = strlen(s2);
+	}
 	s = malloc(((len1 + len2) * sizeof(char)) + 1);
 	if (s == NULL)
 		return (0);
