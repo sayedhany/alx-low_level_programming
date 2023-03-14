6e2ae15470a8cb319be02642bba0bd9197dbdaa1
sayedhany <sayedh@mail.ru>
2023-03-14 02:27:10 -0700

    solved

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * argstostr - concate arguments
+ * @ac: number of arguments
+ * @av: pointer to array of string
+ * Return: string
+ */
+char *argstostr(int ac, char **av)
+{
+	int i, j, size;
+	char *s;
+
+	size = ac - 1;
+	s = malloc(sizeof(char) * size);
+	for (i = 0; i < size; i++)
+	{
+		s[i] = av[i + 1];
+	}
+	s[i] = '\0';
+	return (s);
+}
