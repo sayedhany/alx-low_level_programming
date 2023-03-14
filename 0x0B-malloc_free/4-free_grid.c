44c5928d502dcba58c8121f4d6f7468d320f7cd1
sayedhany <sayedh@mail.ru>
2023-03-14 02:10:58 -0700

    solved

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - free space
+ * @grid: pointer to two dimintional
+ * @height: size of numbers
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+}
