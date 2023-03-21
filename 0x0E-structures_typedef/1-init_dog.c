8ae341056ba05b54a3236b2409b3e854e0bde1ad
sayedhany <sayed591999@gmail.com>
2023-03-21 02:53:39 +0200

    solved

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -7,9 +7,13 @@
  * @age: age
  * @owner: owner name
  */
+
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
