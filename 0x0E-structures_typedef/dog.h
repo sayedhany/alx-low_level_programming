f7888b43a9e316e2f6bd5340084f327e1f119f70
sayedhany <sayed591999@gmail.com>
2023-03-21 02:45:38 +0200

    solved

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,8 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
+
+/**
+ * struct dog - list dates abouts pet
+ * @name: name
+ * @age: age
+ * @owner: owner name
+ */
+
 struct dog {
 	char *name;
 	float age;
 	char *owner;
 }
+
 #endif
