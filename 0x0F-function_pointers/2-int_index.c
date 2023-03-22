02f3f42415ad13bf2566f3d13ce4a37d757f2040
sayedhany <sayedh@mail.ru>
2023-03-22 08:48:38 -0700

    solved

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * int_index - get searched index
+ * @array: array
+ * @size: size of array
+ * @cmp: function
+ * Return: number
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	unsigned int i;
+
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+		else
+			return (-1);
+	}
+}
