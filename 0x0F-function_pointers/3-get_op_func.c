55783ccf6d673800feb56683d5f0c54f80d4950c
sayedhany <sayedh@mail.ru>
2023-03-22 13:29:04 -0700

    solved

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -18,11 +18,11 @@ int (*get_op_func(char *s))(int, int)
 	 };
 	 int i = 0;
 
-	 while (ops[i].op)
-	 {
-		 if (*(ops[i].op) == *s)
-			 return (*(ops[i].f));
-		 i++;
-	 }
-	 return (NULL);
+	while (ops[i].op)
+	{
+		if (*(ops[i].op) == *s)
+			return (*(ops[i].f));
+		i++;
+	}
+	return (NULL);
 }
