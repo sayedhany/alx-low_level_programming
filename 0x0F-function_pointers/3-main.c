368009505d19b60ee21dc1b2d51ced5a92647102
sayedhany <sayedh@mail.ru>
2023-03-22 13:17:37 -0700

    solved

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,39 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check calculation
+ * @argc: number of arguments
+ * @argv: arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2, res;
+	char *op;
+	int (*fun_ptr)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	op = argv[2];
+	while (get_op_func(argv[2]) == NULL || argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if (*argv[3] == '0')
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	fun_ptr = get_op_func(op);
+	res = fun_ptr(num1, num2);
+	printf("%d\n", res);
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,57 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * op_add - add numbers
+ * @a: num1
+ * @b: num2
+ * Return: number
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - sub numbers
+ * @a: num1
+ * @b: num2
+ * Return: number
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - add numbers
+ * @a: num1
+ * @b: num2
+ * Return: number
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - add numbers
+ * @a: num1
+ * @b: num2
+ * Return: number
+ */
+int op_div(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_mod - add numbers
+ * @a: num1
+ * @b: num2
+ * Return: number
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
