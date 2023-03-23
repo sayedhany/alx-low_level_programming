3c61953c28ecb338602a78aba417850a9cfa2c75
sayedhany <sayedh@mail.ru>
2023-03-23 06:27:53 -0700

    solved

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_numbers - print numbers
+ * @separator: char sprate numbers
+ * @n: number of arguments
+ */
+void print_numbers(const char* separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	int value;
+
+	va_list nums;
+	va_start(nums, n);
+	for (i = 0; i < n; i++)
+	{
+		value = va_arg(nums, unsigned int);
+		if (i == (n - 1))
+		{
+			printf("%d", value);
+			break;
+		}
+		printf("%d%s",value, separator); 
+	}
+	va_end(nums);
+	putchar('\n');
+}
