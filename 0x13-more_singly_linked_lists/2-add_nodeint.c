1379968277ea93c9a447c26164f2c5d8a601695a
sayedhany <sayedh@mail.ru>
2023-04-03 13:08:28 -0700

    solved

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * add_nodeint - add node
+ * @head: address of node
+ * @n: new value
+ * Return: address of new element
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new_element;
+	
+	new_element = malloc(sizeof(listint_t));
+	if (new_element == NULL)
+		return (0);
+	new_element->n = n;
+	new_element->next = *head;
+	*head = new_element;
+	return (new_element);
+
+}
