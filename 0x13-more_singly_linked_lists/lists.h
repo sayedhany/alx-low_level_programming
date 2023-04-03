640f119bb901f578f6e47b0a8a66510c7f626db4
sayedhany <sayedh@mail.ru>
2023-04-03 12:34:55 -0700

    solved

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
 #ifndef LISTS_H
-#define LIST_H
+#define LISTS_H
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -10,8 +10,8 @@
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
