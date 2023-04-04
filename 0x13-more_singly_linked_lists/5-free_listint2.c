cc1537dc603502d560ef3ee59e8b43358572bdac
sayedhany <sayedh@mail.ru>
2023-04-04 11:51:36 -0700

    solved

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,16 +6,14 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *curr = *head;
-
 	if (!head || !*head)
 		return;
 
 	while (*head)
 	{
-		listint_t *tem = curr;
-		curr = curr->next;
-		free(temp);
+		listint_t *tem = *head;
+		*head = (*head)->next;
+		free(tem);
 	}
 	*head = NULL;
 }
