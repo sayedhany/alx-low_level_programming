5582d2d657d0244cff7d6b5cf10355bb2c4c460a
sayedhany <sayedh@mail.ru>
2023-04-04 12:32:10 -0700

    solved

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -6,16 +6,21 @@
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	listint_t *node;
+	listint_t *curr = head;
 	unsigned int count;
 
 	count = 0;
-	while (count != index)
-	{
-		node = head->next;
-		count++;
-	}
-	if (index > count)
+	if (!head)
 		return (NULL);
-	return (node);
+
+	while (curr != NULL)
+	{
+		curr = curr->next;
+		count++;
+		if (count == index)
+		{
+			return (curr);
+		}
+	}
+	return (NULL);
 }
