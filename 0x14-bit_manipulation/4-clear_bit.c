a1b4992d9fbec8e211b415f64c3873051adf9b7c
sayedhany <sayedh@mail.ru>
2023-04-08 14:25:08 -0700

    solved

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * clear_bit - set bit
+ * @n: address to number
+ * @index: index
+ * Return: 1 if success -1 if not
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int num;
+
+	num = 1;
+	if (sizeof(n) * 8 < index)
+		return (-1);
+	num = num << index;
+	*n = *n & ~num;
+	return (1);
+}
