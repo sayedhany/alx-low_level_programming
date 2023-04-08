b6fbcefbc38507db001fd980491d2dae77fb9f7e
sayedhany <sayedh@mail.ru>
2023-04-08 14:10:21 -0700

    solved

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * set_bit - set bit
+ * @n: address to number
+ * @index: index
+ * Return: 1 if success -1 if not
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int num;
+	num = 1;
+	if(sizeof(n) * 8 < index)
+		return (-1);
+	num = num << index;
+	*n = *n | num;
+	return (1)
+}
