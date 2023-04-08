50248436f276982c576d3c9a69dcd77c8f8a1f6c
sayedhany <sayedh@mail.ru>
2023-04-08 14:39:52 -0700

    solved

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * flip_bits - flib_bits
+ * @n: number
+ * @m: number
+ * Return: number of bits
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int num;
+	int i = 0;
+	num = n ^ m;
+	while (num >= 1)
+	{
+		if ((num & 1) == 1)
+		i++;
+		num = num >> 1;
+	}
+	return (i);
+}
