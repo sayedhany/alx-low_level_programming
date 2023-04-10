7a2643434726a72e282231aa44f5ec11c958ffb6
sayedhany <sayed591999@gmail.com>
2023-04-11 01:47:45 +0200

    solved

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read-textfile - read text file
+ * @filename: filename
+ * @letters: letters
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t file, letter, write1;
+	char *text;
+
+	text = malloc(letters);
+	if (text == NULL || filename == NULL)
+		return (0);
+	file = open(filename, O_RDONLY);
+	if (file == -1)
+	{
+		free(text);
+		return (0);
+	}
+	letter = read(file, text, letters);
+	write1 = write(STDOUT_FILENO, text, letter);
+	close(file);
+	return (write1);
+}
