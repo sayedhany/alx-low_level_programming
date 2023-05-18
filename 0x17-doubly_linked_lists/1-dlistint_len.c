ab468d7b97063feae6ede377b3ef8c18781feaf0
sayedhany <sayedh@mail.ru>
2023-05-18 10:12:09 -0700

    solved

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * print_dlistint - print number of elements in list
+ * @h: header of list
+ * Return: number on list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	const dlistint_t *current;
+	size_t count;
+
+	current = h;
+	count = 0;
+
+	while (current != NULL)
+	{
+		current = current->next;
+		count++;
+	}
+	return (count);
+}
