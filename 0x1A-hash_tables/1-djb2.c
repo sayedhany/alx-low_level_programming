b97e31e2eeaeaa4d78f1e186d1e068f9e144df08
sayedhany <sayedh@mail.ru>
2023-06-09 16:50:06 -0700

    solveD

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -10,7 +10,8 @@ unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
 	int c;
-	hash = 5382;
+	
+	hash = 5381;
 	while ((c = *str++))
 	{
 		hash = ((hash << 5) + hash) + c; 
