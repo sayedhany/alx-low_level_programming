9ac2362a425f04c81a1c8be9aa0a44fa9124dd11
sayedhany <sayedh@mail.ru>
2023-06-09 17:07:09 -0700

    solveD

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -56,7 +56,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		}
 		else
 		{
-			new_node = ht->array[index];
+			new_node->next = ht->array[index];
 			ht->array[index] = new_node;
 		}
 	}
